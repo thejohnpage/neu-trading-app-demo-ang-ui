44b22de4d218baf070061fc7e034f400054cf95e
John Page <john@thejohnpage.com>
2026-09-17 21:05:43 +0000

    feat: route client markets trade and order history

--- projects/client/src/app/app.routes.ts
+++ projects/client/src/app/app.routes.ts
@@ -1,2 +1,2 @@
-import { Routes } from '@angular/router'; import { DashboardComponent } from './dashboard.component';
-export const routes:Routes=[{path:'',component:DashboardComponent},{path:'**',redirectTo:''}];
+import { Routes } from '@angular/router'; import { DashboardComponent } from './dashboard.component'; import { MarketsComponent } from './markets.component'; import { TradeComponent } from './trade.component'; import { OrdersComponent } from './orders.component';
+export const routes:Routes=[{path:'',component:DashboardComponent},{path:'markets',component:MarketsComponent},{path:'trade',component:TradeComponent},{path:'orders',component:OrdersComponent},{path:'**',redirectTo:''}];
