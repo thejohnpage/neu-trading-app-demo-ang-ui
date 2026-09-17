d3c308515f23c0e7b64a39792aaa9ebcb1b11fb9
John Page <john@thejohnpage.com>
2026-09-17 21:12:17 +0000

    feat: enable admin application routing

--- projects/admin/src/main.ts
+++ projects/admin/src/main.ts
@@ -1,2 +1,2 @@
-import { bootstrapApplication } from '@angular/platform-browser'; import { provideHttpClient } from '@angular/common/http'; import { AppComponent } from './app/app.component';
-bootstrapApplication(AppComponent,{providers:[provideHttpClient()]}).catch(console.error);
+import { bootstrapApplication } from '@angular/platform-browser'; import { provideHttpClient } from '@angular/common/http'; import { provideRouter } from '@angular/router'; import { AppComponent } from './app/app.component'; import { routes } from './app/app.routes';
+bootstrapApplication(AppComponent,{providers:[provideHttpClient(),provideRouter(routes)]}).catch(console.error);
