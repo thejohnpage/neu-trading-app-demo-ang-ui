742327888b952755eab42dfbd8a31c4d8fee5005
John Page <john@thejohnpage.com>
2026-09-21 16:29:49 -0500

    Redesign client portfolio as brokerage account view

--- projects/client/src/app/app.routes.ts
+++ projects/client/src/app/app.routes.ts
@@ -1,2 +1,15 @@
-import { Routes } from '@angular/router'; import { DashboardComponent } from './dashboard.component'; import { MarketsComponent } from './markets.component'; import { TradeComponent } from './trade.component'; import { OrdersComponent } from './orders.component';
-export const routes:Routes=[{path:'',component:DashboardComponent},{path:'markets',component:MarketsComponent},{path:'trade',component:TradeComponent},{path:'orders',component:OrdersComponent},{path:'**',redirectTo:''}];
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
+import { DashboardComponent } from './dashboard.component';
+import { MarketsComponent } from './markets.component';
+import { TradeComponent } from './trade.component';
+import { OrdersComponent } from './orders.component';
+
+export const routes:Routes=[
+ {path:'',component:HomeComponent},
+ {path:'portfolio',component:DashboardComponent},
+ {path:'markets',component:MarketsComponent},
+ {path:'trade',component:TradeComponent},
+ {path:'orders',component:OrdersComponent},
+ {path:'**',redirectTo:''}
+];
