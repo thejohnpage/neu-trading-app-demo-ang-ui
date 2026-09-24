714427026419e70f02b7e0c27cb316a4c48f6bd6
John Page <john@thejohnpage.com>
2026-09-24 14:17:29 -0500

    Add configurable RBAC route

--- projects/admin/src/app/app.routes.ts
+++ projects/admin/src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';import { OverviewComponent } from './overview.component';import { OrdersComponent } from './orders.component';import { ActivityComponent } from './activity.component';import { InstrumentsComponent } from './instruments.component';import { SegmentsComponent } from './segments.component';import { VolumeComponent } from './volume.component';import { UsersComponent } from './users.component';import { AuditComponent } from './audit.component';import { LoginComponent } from '@shared/login.component';import { authGuard } from '@shared/auth.guard';import { capabilityGuard } from '@shared/capability.guard';
+import { Routes } from '@angular/router';import { OverviewComponent } from './overview.component';import { OrdersComponent } from './orders.component';import { ActivityComponent } from './activity.component';import { InstrumentsComponent } from './instruments.component';import { SegmentsComponent } from './segments.component';import { VolumeComponent } from './volume.component';import { RolesComponent } from './roles.component';import { UsersComponent } from './users.component';import { AuditComponent } from './audit.component';import { LoginComponent } from '@shared/login.component';import { authGuard } from '@shared/auth.guard';import { capabilityGuard } from '@shared/capability.guard';
 export const routes:Routes=[
  {path:'login',component:LoginComponent,data:{type:'ADMIN'}},
  {path:'',component:OverviewComponent,canActivate:[authGuard]},
@@ -7,7 +7,7 @@ export const routes:Routes=[
  {path:'instruments',component:InstrumentsComponent,canActivate:[authGuard,capabilityGuard],data:{capability:'REPORTING'}},
  {path:'segments',component:SegmentsComponent,canActivate:[authGuard,capabilityGuard],data:{capability:'REPORTING'}},
  {path:'volume',component:VolumeComponent,canActivate:[authGuard,capabilityGuard],data:{capability:'REPORTING'}},
- {path:'audit',component:AuditComponent,canActivate:[authGuard,capabilityGuard],data:{capability:'AUDIT'}},
- {path:'users',component:UsersComponent,canActivate:[authGuard,capabilityGuard],data:{capability:'USER_MANAGEMENT'}},
+ {path:'audit',component:AuditComponent,canActivate:[authGuard,capabilityGuard],data:{capability:'AUDIT_VIEW'}},
+ {path:'users',component:UsersComponent,canActivate:[authGuard,capabilityGuard],data:{capability:'USER_MANAGEMENT'}},{path:'roles',component:RolesComponent,canActivate:[authGuard,capabilityGuard],data:{capability:'ROLE_MANAGEMENT'}},
  {path:'**',redirectTo:''}
 ];
